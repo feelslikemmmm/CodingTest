2c5369946c167f86cb2df0ca8d1366444bcb66de
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2020-09-25 11:40:58 +0900

    고차함수 / 11_keep

---
+++ 고차함수/01_returnFunction.js
@@ -0,0 +1,10 @@
+// 'Hello HoF!' 리턴하는 함수를 리턴해야 합니다.
+//입력 : 없음
+//출력 : 함수를 리턴해야 합니다
+//주의사항 : 리턴하는 함수의 입력값은 없습니다.
+    function returnFunction() {
+      // TODO: 여기에 코드를 작성합니다.
+      return () => {
+        return 'Hello HoF!';
+      }
+    }
