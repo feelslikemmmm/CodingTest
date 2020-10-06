f6127addddce70108045b80563cb437fb2dcc838
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2020-10-06 10:48:34 +0900

    알고리즘베이직 / 03_powerOfTwo

---
+++ Algorithm Basic/03_powerOfTwo.js
@@ -0,0 +1,23 @@
+//문제) 수를 입력받아 2의 거듭제곱인지 여부를 리턴해야 합니다.
+//입력) 인자 1 : num number 타입의 정수 (num >= 1)
+//출력) boolean 타입을 리턴해야 합니다.
+//주의 사항) 반복문(while)문을 사용해야 합니다.
+//2의 0승은 1입니다.
+//Number.isInteger, Math.log2, Math.log 사용은 금지됩니다.
+function powerOfTwo(num) {
+  //TODO: 여기에 코드를 작성합니다.
+  if (num === 1) {
+    return true;
+  }
+
+  if (num % 2 ) {
+    return false;
+  }
+
+  let num2 = 2;
+  while (num2 < num) {
+    num2 = num2 * 2;
+  }
+
+  return num2 === num;
+}
