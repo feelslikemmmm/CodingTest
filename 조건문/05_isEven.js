0a7feff10f7f69c6cf407c5d2c6fa42f9d40f898
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2020-10-06 15:50:46 +0900

    조건문 / 05_isEven

---
+++ 조건문/05_isEven.js
@@ -0,0 +1,13 @@
+
+//문제 ) 수를 입력받아 짝수인지 여부를 리턴해야 합니다.
+//입력 ) 인자 1: num (number 타입의 정수 (num >= 0)
+//출력 ) boolean 타입을 리턴해야 합니다
+//0은 짝수로 간주합니다
+function isEven(num) {
+	// TODO: 여기에 코드를 작성합니다.
+	if(num % 2) {
+		return false;
+	} else {
+		return true;
+	}
+}
