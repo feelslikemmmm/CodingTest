2bb53622c5d602c942a3a17e3b9b7f1d5a4d4117
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2020-10-07 10:15:40 +0900

    알고리즘 베이직 / 11_removeExtremes

--- Algorithm Basic/11_removeExtremes.js
+++ Algorithm Basic/11_removeExtremes.js
@@ -3,8 +3,6 @@
 //출력) 배열을 리턴해야 합니다.
 //주의 사항) 가장 짧은 문자열의 길이와 가장 긴 문자열의 길이가 같은 경우는 없습니다.
 //가장 짧은 문자열 또는 가장 긴 문자열이 다수일 경우, 나중에 위치한 문자열을 제거합니다.
-function insertDash(str) {
-  // TODO: 여기에 코드를 작성합니다.
   function removeExtremes(arr) {
     // TODO: 여기에 코드를 작성합니다.
     let maxLength = 20;
@@ -32,5 +30,3 @@ function insertDash(str) {
     }
     return result;
   }
-  
-}
